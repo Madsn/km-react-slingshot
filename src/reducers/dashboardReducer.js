5673e877ae7915bd8a84d4bb5e9c1fd0338d00cd
Madsn <m@madsn.net>
2016-09-14 00:39:57 +0200

    Refactoring actions for async api call

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,7 +1,15 @@
+import * as actionTypes from '../constants/actionTypes';
+import objectAssign from 'object-assign';
 import initialState from './initialState';
 
 export default function dashboardReducer(state = initialState.bookings, action) {
   switch (action.type) {
+    case actionTypes.FETCHING_BOOKINGS:
+      return objectAssign({}, state, {loading: true, error: undefined});
+    case actionTypes.RECEIVE_BOOKINGS:
+      return objectAssign({}, state, {bookings: action.bookings, loading: false, error: undefined});
+    case actionTypes.API_ERROR:
+      return objectAssign({}, state, {loading: true, loading: false, error: action.error});
     default:
       return state;
   }
