9ab56b6b8c00d96436d19fb9299237d932e389b4
Madsn <m@madsn.net>
2016-09-14 23:14:05 +0200

    Add bookings route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
-import DashboardPage from './containers/DashboardPage'; // eslint-disable-line import/no-named-as-default
+import DashboardPage from './components/dashboard/DashboardPage'; // eslint-disable-line import/no-named-as-default
+import ViewBookingPage from './components/bookings/ViewBookingPage'; // eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="dashboard" component={DashboardPage}/>
+    <Route path="bookings/:bookingId" component={ViewBookingPage}/>
   </Route>
 );
